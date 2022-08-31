2850d67608ff12632cdb3e9c0ec60af8f4af80cc
Aldik <andi.kitta77@gmail.com>
2022-08-31 15:15:59 +0800

    NoteRepositoryImpl

---
+++ app/src/main/java/com/aldikitta/crudnoteapp/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.aldikitta.crudnoteapp.feature_note.data.repository
+
+import com.aldikitta.crudnoteapp.feature_note.data.data_source.NoteDao
+import com.aldikitta.crudnoteapp.feature_note.domain.model.Note
+import com.aldikitta.crudnoteapp.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val dao: NoteDao
+) : NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return dao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return dao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        return insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        return deleteNote(note)
+    }
+}
