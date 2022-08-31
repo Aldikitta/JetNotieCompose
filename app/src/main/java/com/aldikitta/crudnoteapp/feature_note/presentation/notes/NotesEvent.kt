6a44fa94ccf79d43799faf8f20b159f3bdc34137
Aldik <andi.kitta77@gmail.com>
2022-08-31 17:58:26 +0800

    NotesEvent

---
+++ app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/notes/NotesEvent.kt
@@ -0,0 +1,11 @@
+package com.aldikitta.crudnoteapp.feature_note.presentation.notes
+
+import com.aldikitta.crudnoteapp.feature_note.domain.model.Note
+import com.aldikitta.crudnoteapp.feature_note.domain.util.NoteOrder
+
+sealed class NotesEvent {
+    data class Order(val noteOrder: NoteOrder) : NotesEvent()
+    data class DeleteNote(val note: Note) : NotesEvent()
+    object RestoreNote : NotesEvent()
+    object ToggleOrderSection : NotesEvent()
+}
---
+++ app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/notes/NotesUiState.kt
@@ -0,0 +1,11 @@
+package com.aldikitta.crudnoteapp.feature_note.presentation.notes
+
+import com.aldikitta.crudnoteapp.feature_note.domain.model.Note
+import com.aldikitta.crudnoteapp.feature_note.domain.util.NoteOrder
+import com.aldikitta.crudnoteapp.feature_note.domain.util.OrderType
+
+data class NotesUiState(
+    val notes: List<Note> = emptyList(),
+    val noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending),
+    val isOrderSectionVisible: Boolean = false
+)
