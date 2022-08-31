3f634b923186333d87e2e6f86cb45bb1fa17812a
Aldik <andi.kitta77@gmail.com>
2022-08-31 15:13:40 +0800

    NoteRepository

---
+++ app/src/main/java/com/aldikitta/crudnoteapp/feature_note/domain/repository/NoteRepository.kt
@@ -0,0 +1,14 @@
+package com.aldikitta.crudnoteapp.feature_note.domain.repository
+
+import com.aldikitta.crudnoteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+    fun getNotes(): Flow<List<Note>>
+
+    suspend fun getNoteById(id: Int): Note?
+
+    suspend fun insertNote(note: Note)
+
+    suspend fun deleteNote(note: Note)
+}
