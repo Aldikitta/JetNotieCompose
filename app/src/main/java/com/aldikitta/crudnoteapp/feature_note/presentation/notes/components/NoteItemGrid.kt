3310217be853cffd497c36ade610ae72b37ac773
Aldik <andi.kitta77@gmail.com>
2022-09-13 14:15:05 +0800

    Staggered Grid View

--- app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/notes/components/NoteItemGrid.kt
+++ app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/notes/components/NoteItemGrid.kt
@@ -1,187 +1,177 @@
-//package com.aldikitta.crudnoteapp.feature_note.presentation.notes.components
-//
-//import androidx.compose.runtime.Composable
-//
-//@Composable
-//fun StaggeredGridView() {
-//    // on below line we are creating
-//    // an array of images.
-//    val images = listOf(
-//        R.drawable.imgone,
-//        R.drawable.imgtwo,
-//        R.drawable.imgthree,
-//        R.drawable.imgfour,
-//        R.drawable.imgfive,
-//        R.drawable.imgsix,
-//        R.drawable.imgseven,
-//        R.drawable.imgeight,
-//        R.drawable.imgnine,
-//        R.drawable.imgten
-//    )
-//
-//    // on below line we are creating a column
-//    // for our staggered grid view.
-//    Column(
-//        // for this column we are adding a
-//        // modifier to it to fill max size.
-//        modifier = Modifier
-//            .fillMaxSize()
-//    ) {
-//        // on below line we are creating a column
-//        // for each item of our staggered grid.
-//        Column(
-//            // in this column we are adding modifier to it
-//            // and adding padding from all sides and vertical scroll.
-//            modifier = Modifier
-//                .verticalScroll(rememberScrollState())
-//                .padding(5.dp)
-//        ) {
-//            // on below line we are calling our
-//            // custom staggered vertical grid item.
-//            CustomStaggeredVerticalGrid(
-//                // on below line we are specifying
-//                // number of columns for our grid view.
-//                numColumns = 2,
-//
-//                // on below line we are adding padding
-//                // from all sides for our grid view.
-//                modifier = Modifier.padding(5.dp)
-//            ) {
-//                // inside staggered grid view we are
-//                // adding images for each item of grid.
-//                images.forEach { img ->
-//                    // on below line inside our grid
-//                    // item we are adding card.
-//                    Card(
-//                        // on below line inside the card we
-//                        // are adding modifier to it to specify
-//                        // max width, padding, elevation and shape for the card
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .padding(5.dp),
-//                        elevation = 10.dp,
-//                        shape = RoundedCornerShape(10.dp)
-//                    ) {
-//                        // on below line we are adding column inside our card.
-//                        Column(
-//                            // in this column we are adding modifier
-//                            // to fill max size and align our
-//                            // card center horizontally.
-//                            modifier = Modifier
-//                                .fillMaxSize()
-//                                .align(Alignment.CenterHorizontally),
-//                            horizontalAlignment = Alignment.CenterHorizontally
-//                        ) {
-//                            // inside our column we are creating an image.
-//                            Image(
-//                                // on below line we are specifying the
-//                                // drawable image for our image.
-//                                painterResource(id = img),
-//
-//                                // on below line we are specifying
-//                                // content description for our image
-//                                contentDescription = "images",
-//
-//                                // on below line we are specifying
-//                                // alignment for our image.
-//                                alignment = Alignment.Center,
-//                            )
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//// on below line we are creating a custom
-//// composable item for our grid view item.
-//@Composable
-//fun CustomStaggeredVerticalGrid(
-//    // on below line we are specifying
-//    // parameters as modifier, num of columns
-//    modifier: Modifier = Modifier,
-//    numColumns: Int = 2,
-//    content: @Composable () -> Unit
-//) {
-//    // inside this grid we are creating
-//    // a layout on below line.
-//    Layout(
-//        // on below line we are specifying
-//        // content for our layout.
-//        content = content,
-//        // on below line we are adding modifier.
-//        modifier = modifier
-//    ) { measurable, constraints ->
-//        // on below line we are creating a variable for our column width.
-//        val columnWidth = (constraints.maxWidth / numColumns)
-//
-//        // on the below line we are creating and initializing our items constraint widget.
-//        val itemConstraints = constraints.copy(maxWidth = columnWidth)
-//
-//        // on below line we are creating and initializing our column height
-//        val columnHeights = IntArray(numColumns) { 0 }
-//
-//        // on below line we are creating and initializing placebles
-//        val placeables = measurable.map { measurable ->
-//            // inside placeble we are creating
-//            // variables as column and placebles.
-//            val column = testColumn(columnHeights)
-//            val placeable = measurable.measure(itemConstraints)
-//
-//            // on below line we are increasing our column height/
-//            columnHeights[column] += placeable.height
-//            placeable
-//        }
-//
-//        // on below line we are creating a variable for
-//        // our height and specifying height for it.
-//        val height =
-//            columnHeights.maxOrNull()?.coerceIn(constraints.minHeight, constraints.maxHeight)
-//                ?: constraints.minHeight
-//
-//        // on below line we are specifying height and width for our layout.
-//        layout(
-//            width = constraints.maxWidth,
-//            height = height
-//        ) {
-//            // on below line we are creating a variable for column y pointer.
-//            val columnYPointers = IntArray(numColumns) { 0 }
-//
-//            // on below line we are setting x and y for each placeable item
-//            placeables.forEach { placeable ->
-//                // on below line we are calling test
-//                // column method to get our column index
-//                val column = testColumn(columnYPointers)
-//
-//                placeable.place(
-//                    x = columnWidth * column,
-//                    y = columnYPointers[column]
-//                )
-//
-//                // on below line we are setting
-//                // column y pointer and incrementing it.
-//                columnYPointers[column] += placeable.height
-//            }
-//        }
-//    }
-//}
-//
-//// on below line we are creating a test column method for setting height.
-//private fun testColumn(columnHeights: IntArray): Int {
-//    // on below line we are creating a variable for min height.
-//    var minHeight = Int.MAX_VALUE
-//
-//    // on below line we are creating a variable for column index.
-//    var columnIndex = 0
-//
-//    // on below line we are setting column  height for each index.
-//    columnHeights.forEachIndexed { index, height ->
-//        if (height < minHeight) {
-//            minHeight = height
-//            columnIndex = index
-//        }
-//    }
-//    // at last we are returning our column index.
-//    return columnIndex
-//}
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.aldikitta.crudnoteapp.feature_note.presentation.notes.NotesUiState
+import com.aldikitta.crudnoteapp.ui.theme.spacing
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+@Composable
+fun NoteItemGrid(
+    state: NotesUiState,
+    paddingValues: PaddingValues?
+){
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(paddingValues!!)
+    ){
+        CustomStaggeredVerticalGrid(
+            numColumns = 2,
+        ) {
+            state.notes.forEach { note ->
+                var time = note.timeStamp
+                var times = SimpleDateFormat("MMM d, yyyy")
+                var date = Date(time)
+                var final = times.format(date)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(MaterialTheme.spacing.small)
+//                                    .heightIn(min = 0.dp, max = 300.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(MaterialTheme.colorScheme.inverseOnSurface)
+                                .padding(MaterialTheme.spacing.small)
+//                                    .align(Alignment.CenterHorizontally),
+//                                horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Row(
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text(
+                                    text = note.title,
+                                    style = MaterialTheme.typography.titleLarge,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier.padding(bottom = MaterialTheme.spacing.small)
+                                )
+                                Box(
+                                    modifier = Modifier
+                                        .clip(CircleShape)
+//                                            .weight(1f)
+                                        .size(20.dp)
+                                        .background(Color(note.color))
+                                )
+                            }
+                            Text(text = note.content)
+                        }
+                    }
+                    Text(
+                        text = final,
+                        modifier = Modifier.padding(bottom = MaterialTheme.spacing.large)
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun CustomStaggeredVerticalGrid(
+    // on below line we are specifying
+    // parameters as modifier, num of columns
+    modifier: Modifier = Modifier,
+    numColumns: Int = 2,
+    content: @Composable () -> Unit
+) {
+    // inside this grid we are creating
+    // a layout on below line.
+    Layout(
+        // on below line we are specifying
+        // content for our layout.
+        content = content,
+        // on below line we are adding modifier.
+        modifier = modifier
+    ) { measurable, constraints ->
+        // on below line we are creating a variable for our column width.
+        val columnWidth = (constraints.maxWidth / numColumns)
+
+        // on the below line we are creating and initializing our items constraint widget.
+        val itemConstraints = constraints.copy(maxWidth = columnWidth)
+
+        // on below line we are creating and initializing our column height
+        val columnHeights = IntArray(numColumns) { 0 }
+
+        // on below line we are creating and initializing placebles
+        val placeables = measurable.map { measurable ->
+            // inside placeble we are creating
+            // variables as column and placebles.
+            val column = testColumn(columnHeights)
+            val placeable = measurable.measure(itemConstraints)
+
+            // on below line we are increasing our column height/
+            columnHeights[column] += placeable.height
+            placeable
+        }
+
+        // on below line we are creating a variable for
+        // our height and specifying height for it.
+        val height =
+            columnHeights.maxOrNull()?.coerceIn(constraints.minHeight, constraints.maxHeight)
+                ?: constraints.minHeight
+
+        // on below line we are specifying height and width for our layout.
+        layout(
+            width = constraints.maxWidth,
+            height = height
+        ) {
+            // on below line we are creating a variable for column y pointer.
+            val columnYPointers = IntArray(numColumns) { 0 }
+
+            // on below line we are setting x and y for each placeable item
+            placeables.forEach { placeable ->
+                // on below line we are calling test
+                // column method to get our column index
+                val column = testColumn(columnYPointers)
+
+                placeable.place(
+                    x = columnWidth * column,
+                    y = columnYPointers[column]
+                )
+
+                // on below line we are setting
+                // column y pointer and incrementing it.
+                columnYPointers[column] += placeable.height
+            }
+        }
+    }
+}
+
+// on below line we are creating a test column method for setting height.
+private fun testColumn(columnHeights: IntArray): Int {
+    // on below line we are creating a variable for min height.
+    var minHeight = Int.MAX_VALUE
+
+    // on below line we are creating a variable for column index.
+    var columnIndex = 0
+
+    // on below line we are setting column  height for each index.
+    columnHeights.forEachIndexed { index, height ->
+        if (height < minHeight) {
+            minHeight = height
+            columnIndex = index
+        }
+    }
+    // at last we are returning our column index.
+    return columnIndex
+}
--- app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/search/SearchScreen.kt
@@ -1,12 +1,18 @@
 package com.aldikitta.crudnoteapp.feature_note.presentation.search
 
+import CustomStaggeredVerticalGrid
+import NoteItemGrid
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Person
@@ -15,13 +21,16 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.R
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -30,6 +39,9 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.aldikitta.crudnoteapp.feature_note.presentation.notes.NotesEvent
 import com.aldikitta.crudnoteapp.feature_note.presentation.notes.NotesViewModel
+import com.aldikitta.crudnoteapp.ui.theme.spacing
+import java.text.SimpleDateFormat
+import java.util.*
 
 @OptIn(ExperimentalLifecycleComposeApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -45,36 +57,19 @@ fun SearchScreen(
         var query by rememberSaveable {
             mutableStateOf("")
         }
-        LaunchedEffect(query){viewModel.onEvent(NotesEvent.SearchNotes(query))}
+        LaunchedEffect(query) { viewModel.onEvent(NotesEvent.SearchNotes(query)) }
         val focusRequester = remember { FocusRequester() }
-        LaunchedEffect(true){focusRequester.requestFocus()}
+        LaunchedEffect(true) { focusRequester.requestFocus() }
         OutlinedTextField(
             value = query,
-            onValueChange = {query = it},
-            label = {Text(text = "Search")},
+            onValueChange = { query = it },
+            label = { Text(text = "Search") },
             shape = RoundedCornerShape(15.dp),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
                 .focusRequester(focusRequester)
         )
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(top = 12.dp, bottom = 12.dp)
-        ) {
-            items(state.notes, key = {it.id!!}){ task ->
-                NoteItem(
-                    task = task,
-                    onClick = {
-//                        navController.navigate(
-//                            Screen.TaskDetailScreen.route.replace(
-//                                "{${Constants.TASK_ID_ARG}}",
-//                                "${task.id}"
-//                            )
-//                        )
-                    },
-                )
-            }
-        }
+        NoteItemGrid(state = state, paddingValues = null)
     }
 }
