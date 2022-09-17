1a84f74af969cc18322351609fc18528d1d37d01
Aldik <andi.kitta77@gmail.com>
2022-09-17 18:04:08 +0800

    finishing

--- app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
+++ app/src/main/java/com/aldikitta/crudnoteapp/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
